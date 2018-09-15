4e0eaebeffa8678ab59fc3c04428e3f35ba8ced5
Obydullah Shishir <shishir.srdr16@gmail.com>
2018-09-16 03:00:03 +0600

    Bug Fixed

---
+++ index.php
@@ -0,0 +1,372 @@
+<?php require_once 'includes/header.php'; ?>
+
+
+
+    <header class="mb-5" id="home">
+      <nav class="navbar navbar-light bg-light navbar-expand-md fixed-top ">
+      <a href="#" class="navbar-brand">BD Lab IT</a>
+      <button type="button" class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon text-dark"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto" id="navScrollspy">
+          <li class="nav-item active">
+            <a class="nav-link" href="../bdLabIT">
+              <span class="navbar-text ">Home</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="services">
+              <span class="navbar-text">Services</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="project">
+              <span class="navbar-text">Project</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="news">
+              <span class="navbar-text">Newsroom</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="career">
+              <span class="navbar-text">Career</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about">
+              <span class="navbar-text">About Us</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <section id="section-jumbotron" class="jumbotron jumbotron-fluid text-white d-flex justify-content-center align-items-center">
+      <div class="container text-center">
+        <p class="display-4 d-none d-sm-block">bdLabIT</p>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, quos.</p>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, corrupti?</p>
+        <p><strong>Download now on:</strong></p>
+        <a href="#" class="btn btn-lg btn-primary mb-2"><i class="fab fa-apple" aria-hidden="true"></i> App Store</a>
+        <a href="#" class="btn btn-lg btn-primary mb-2"><i class="fab fa-android" aria-hidden="true"></i> Google Play</a>
+      </div>
+    </section>
+  </header> <!-- Header End -->
+
+
+    <div class="container text-center" >
+      <section id="section-description">
+        <h2 class="display-4">What is BDLabIT ?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, excepturi. Consequuntur asperiores distinctio praesentium quas, iste repellat, assumenda? Veniam quaerat ducimus, nesciunt ullam debitis alias consectetur incidunt, facere minima repudiandae repellendus quod unde quisquam tempora eum dolorum harum fuga at magni numquam sit vel! Quia eveniet odio tempore at, ipsam laboriosam debitis vitae est sed natus ad dignissimos, amet maiores.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quisquam, aliquid iste, repellat incidunt aut, et excepturi labore, aperiam distinctio porro sint soluta eius consectetur! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quisquam, aliquid iste, repellat incidunt aut.</p>
+      </section>
+    </div>
+
+
+    <div class="container-fluid bg-info">
+      <div class="row pt-3">
+        <div class="col-12">
+          <div class="feature_heading">
+          <h2 class="display-4 text-center">Feature</h2>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row pb-4">
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="far fa-images fa-4x"></i></i></h5>
+                  <h4>Graphics Design</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fas fa-laptop-code fa-4x"></i></h5>
+                  <h4>Web Development</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fab fa-android fa-4x"></i></i></h5>
+                  <h4>Android Apps</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fab fa-apple fa-4x"></i></h5>
+                  <h4>iOS Apps<sup>*</sup></h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  <!-- Feature End -->
+
+
+    <div class="container my-5">
+      <section id="section-description ">
+        <h2 class="display-4 text-center">Our Mission</h2>
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, excepturi. Consequuntur asperiores distinctio praesentium quas, iste repellat, assumenda? Veniam quaerat ducimus, nesciunt ullam debitis alias consectetur incidunt, facere minima repudiandae repellendus quod unde quisquam tempora eum dolorum harum fuga at magni numquam sit vel! Quia eveniet odio tempore at, ipsam laboriosam debitis vitae est sed natus ad dignissimos, amet maiores.</p>
+        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quisquam, aliquid iste, repellat incidunt aut, et excepturi labore, aperiam distinctio porro sint soluta eius consectetur! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis quisquam, aliquid iste, repellat incidunt aut.</p>
+      </section>
+    </div>
+
+
+    <div class="container-fluid text-white pb-5" style="background-color: #082339">
+      <div class="row pt-3">
+        <div class="col-12">
+          <div class="feature_heading">
+          <h2 class="display-4 text-center pt-3">Why Choose Us?</h2>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 text-justify">
+          <h3>Reason</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed minima veniam blanditiis adipisci! Animi delectus corporis voluptatibus voluptatem, aliquam eveniet non debitis nesciunt rem natus.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dignissimos totam corrupti maxime velit excepturi animi suscipit sint eligendi quisquam beatae rerum, nulla ipsam accusantium. Unde rerum nisi eligendi aperiam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel quasi, et nihil nostrum praesentium reprehenderit quae, voluptas voluptates ratione placeat, commodi minima unde corporis excepturi eum voluptate impedit eius provident ea fugiat in amet. Modi necessitatibus inventore vero molestias, odio ea, voluptate. Ducimus, quas debitis. Facere nihil accusanti.</p>
+        </div>
+        <div class="col-md-6">
+          <h3 class="text-right">Skills</h3>
+
+          <table class="table table-bordered table-active mt-3 rounded-circle">
+            <tr>
+              <td class="text-right" width="20%">HTML</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: 98%;" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100">98%</div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-right" width="20%">CSS</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-right" width="20%">jQuery</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-right" width="20%">PHP</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-right" width="20%">Graphics</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 88%;" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100">88%</div>
+                </div>
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-right" width="20%">Apps</td>
+              <td>
+                <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%</div>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
+        </div>
+      </div>
+    </div>  <!-- Why Choose Us End -->
+
+
+    <div class="container-fluid text-white pb-5" style="background-color: #CCCCCC">
+      <div class="row pt-3">
+        <div class="col-12">
+          <div class="feature_heading">
+          <h2 class="display-4 text-center pt-3 text-dark">News &amp Events</h2>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row" style="margin-top: 40px;">
+          <div class="col-md-6 text-justify mb-2" >
+            <div class="card">
+              <img class="card-img-top img-fluid" src="img/officials.jpg" height="350px" alt="Card image cap">
+              <div class="card-body text-dark">
+                <h5 class="card-title">Event Title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+          <div class="single_news bg-info p-3 mb-2 rounded">
+            <h4>News Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit voluptate, unde ut officiis, eveniet quam! Autem voluptates dolore, sequi optio.</p>
+          </div>
+
+          <div class="single_news bg-info p-3 my-2 rounded">
+            <h4>News Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit voluptate, unde ut officiis, eveniet quam! Autem voluptates dolore, sequi optio.</p>
+          </div>
+
+          <div class="single_news bg-info p-3 my-2 rounded">
+            <h4>News Title</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit voluptate, unde ut officiis, eveniet quam! Autem voluptates dolore, sequi optio.</p>
+          </div>
+          <a href="news.php" class="btn btn-primary form-control">View All</a>
+        </div>
+        </div>
+      </div>
+    </div>  <!-- News & Events End -->
+
+    <div class="container-fluid px-0">
+
+      <section class="testimonials py-5 text-white px-1 px-md-5 margin-top-xl">
+        <img src="img/solodev-logo-stacked.png" class="icon-overlay img-fluid" />
+        <h2 class="display-4 text-center pb-3">Testimonials</h2>
+
+        <div class="col-sm-12">
+          <div class="carousel-controls testimonial-carousel-controls">
+            <div class="control d-flex align-items-center justify-content-center prev" style="margin-top:150px;"><i class="fa fa-chevron-left"></i></div>
+            <div class="control d-flex align-items-center justify-content-center next" style="margin-top:150px;"><i class="fa fa-chevron-right"></i></div>
+
+            <div class="testimonial-carousel">
+              <div class="h5 font-weight-normal one-slide mx-auto">
+                <div class="testimonial w-100 px-3 text-center d-flex flex-direction-column justify-content-center flex-wrap align-items-center">
+                  <img src="img/wz.jpg" class="rounded-circle mb-2">
+                  <div class="message text-center blockquote w-100">"They’ve been consistent throughout the years and grown together with us. Even as they’ve grown, they haven’t lost sight of what they do. Most of their key resources are still with them, which is also a testament to their organization."</div>
+                  <div class="blockquote-footer w-100 text-white lead font-weight-bold">Dr. Wahiduzzaman Ripon</div>
+                  <div class="text-white w-100"><small>Associate Professor</small></div>
+                  <div class="text-white w-100">IIT, Jahangirnagar University, Savar, Dhaka</div>
+                </div>
+              </div>
+              <div class="h5 font-weight-normal one-slide mx-auto">
+                <div class="testimonial w-100 px-3 text-center d-flex flex-direction-column justify-content-center flex-wrap align-items-center">
+                  <img src="img/wz.jpg" class="rounded-circle mb-2">
+                  <div class="message text-center blockquote w-100">"They’ve been consistent throughout the years and grown together with us. Even as they’ve grown, they haven’t lost sight of what they do. Most of their key resources are still with them, which is also a testament to their organization."</div>
+                  <div class="blockquote-footer w-100 text-white lead font-weight-bold">Dr. Wahiduzzaman Ripon</div>
+                  <div class="text-white w-100"><small>Associate Professor</small></div>
+                  <div class="text-white w-100">IIT, Jahangirnagar University, Savar, Dhaka</div>
+                </div>
+              </div>
+              <div class="h5 font-weight-normal one-slide mx-auto">
+                <div class="testimonial w-100 px-3 text-center d-flex flex-direction-column justify-content-center flex-wrap align-items-center">
+                  <img src="img/wz.jpg" class="rounded-circle mb-2">
+                  <div class="message text-center blockquote w-100">"They’ve been consistent throughout the years and grown together with us. Even as they’ve grown, they haven’t lost sight of what they do. Most of their key resources are still with them, which is also a testament to their organization."</div>
+                  <div class="blockquote-footer w-100 text-white lead font-weight-bold">Dr. Wahiduzzaman Ripon</div>
+                  <div class="text-white w-100"><small>Associate Professor</small></div>
+                  <div class="text-white w-100">IIT, Jahangirnagar University, Savar, Dhaka</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>  <!-- Testimonials End -->
+
+
+    <div class="container-fluid bg-info">
+      <div class="row pt-3">
+        <div class="col-12">
+          <div class="feature_heading">
+          <h2 class="display-4 text-center text-white">Summary</h2>
+        </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row pb-4">
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fas fa-unlock fa-3x"></i></h5>
+                  <h4>Complete Project</h4>
+                  <h2>10+</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fas fa-users fa-3x"></i></h5>
+                  <h4>Total Clients</h4>
+                  <h2>21+</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fas fa-lock fa-3x"></i></h5>
+                  <h4>Processing</h4>
+                  <h2>5+</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-3 py-4">
+            <div class="single_feature text-center">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title"><i class="fas fa-people-carry fa-3x"></i></i></h5>
+                  <h4>Team Members</h4>
+                  <h2>15</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  <!-- Summery End -->
+
+
+
+
+
+<?php require_once 'includes/footer.php'; ?>
+
+
+
+
+
